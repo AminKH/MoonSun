16cd16c251c29e1f5675ab13dd683ab92071142b
Amin <akhiabani57@gmail.com>
2021-01-20 18:56:13 +0330

    Update staticMoonSunCDLL.bat

--- Fortran/staticMoonSunCDLL.bat
+++ Fortran/staticMoonSunCDLL.bat
@@ -1,2 +1,4 @@
-mingw32-gfortran.exe -Wall -DBUILD_DLL -O2  -ffree-line-length-none -Wall -c MoonSun.f95 -o MoonSun.o
-mingw32-gfortran -shared -static -Wl,--output-def=StaticMoonSunC.def -Wl,--out-implib=StaticMoonSunC.a -Wl,--dll MoonSun.o -o StaticMoonSunC.dll -s
+gfortran.exe -Wall -DBUILD_DLL -O2  -ffree-line-length-none -Wall -c MoonSun.f90 -o MoonSun.o
+gfortran.exe -Wall -DBUILD_DLL -O2  -ffree-line-length-none -Wall -c SOFA.f90 -o SOFA.o
+gfortran.exe -Wall -DBUILD_DLL -O2  -ffree-line-length-none -Wall -c NOVAS.f90 -o NOVAS.o
+gfortran -shared -static -Wl,--output-def=StaticMoonSunC.def -Wl,--out-implib=StaticMoonSunC.a -Wl,--dll MoonSun.o SOFA.o NOVAS.o -o StaticMoonSunC.dll -s
