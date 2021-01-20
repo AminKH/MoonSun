4bc107aee5ff05aa4a366ad0504b211743ad74c0
Amin <akhiabani57@gmail.com>
2021-01-20 18:55:02 +0330

    Update StaticMoonSun.bat

--- Fortran/StaticMoonSun.bat
+++ Fortran/StaticMoonSun.bat
@@ -1,5 +1,7 @@
-mingw32-gfortran -Wall  -O2  -ffree-line-length-none -Wall -c Main.f95
-mingw32-gfortran -Wall  -O2  -ffree-line-length-none -Wall -c MoonSun.f95
-mingw32-gfortran -Wall  -O2  -ffree-line-length-none -Wall -c MSISGTD.FOR
-mingw32-gfortran Main.o MoonSun.o MSISGTD.o -Wextra -Wall -pedantic -ffree-form -static -o Calendar.exe -s
+gfortran -Wall  -O2  -ffree-line-length-none -Wall -c SOFA.f90
+gfortran -Wall  -O2  -ffree-line-length-none -Wall -c NOVAS.f90
+gfortran -Wall  -O2  -ffree-line-length-none -Wall -c MoonSun.f90
+gfortran -Wall  -O2  -ffree-line-length-none -Wall -c MSISGTD.FOR
+gfortran -Wall  -O2  -ffree-line-length-none -Wall -c Main.f90
+gfortran SOFA.o NOVAS.o MSISGTD.o MoonSun.o Main.o -Wextra -Wall -pedantic -ffree-form -static -o Calendar.exe -s
 
